4724dd29bf223db02b27104cc1efce352d8548bf
minhchi1509 <minhchico300kc@gmail.com>
2023-01-10 22:43:25 +0700

    add features

--- src/page/Home/components/SearchStudent.jsx
+++ src/page/Home/components/SearchStudent.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { OutlinedInput, InputAdornment } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search';
 
+import FormModal from './FormModal';
+
 export default function SearchStudent() {
+    const formModalRef = useRef(null);
+
+    const handleSearchStudent = (values) => {
+        console.log(values);
+    }
+
     return (
-        <OutlinedInput
-            size='small'
-            className='rounded-full bg-grey-200 dark:bg-grey-900'
-            placeholder='Tìm kiếm...'
-            startAdornment={
-                <InputAdornment position='start'>
-                    <SearchIcon />
-                </InputAdornment>
-            }
-            fullWidth
-        />
+        <>
+            <OutlinedInput
+                fullWidth
+                size='small'
+                className='rounded-full bg-grey-200 dark:bg-grey-900'
+                placeholder='Tìm kiếm...'
+                startAdornment={
+                    <InputAdornment position='start'>
+                        <SearchIcon />
+                    </InputAdornment>
+                }
+                onClick={() => formModalRef.current.show()}
+            />
+            <FormModal
+                ref={formModalRef}
+                type='search'
+                title='Tìm kiếm sinh viên'
+                buttonName='Tìm kiếm'
+                handleSubmit={handleSearchStudent}
+            />
+        </>
     )
 }
